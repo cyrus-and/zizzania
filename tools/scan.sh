75f906a678a2b4f6bd4f718379b5b808d10657ce
Andrea Cardaci <cyrus.and@gmail.com>
2013-10-24 00:01:07 +0200

    Filter scan tool results

--- tools/scan.sh
+++ tools/scan.sh
@@ -3,4 +3,4 @@ USAGE='Usage: scan.sh'
 trap 'echo "$USAGE"; exit 1' ERR
 
 DEVICE=${DEVICE:-wlan0}
-iw dev $DEVICE scan 2>&1 | less
+iw dev $DEVICE scan 2>&1 | grep $'^BSS\|^\tsignal\|^\tSSID\|channel [0-9]\+' | less
