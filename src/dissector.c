cyrus-and/zizzania

commit ccd0a66611dc59c8a92cd947eb600a08193ef9f0

Show unwanted BSSID messages in debug mode only

src/dissector.c
@@ -127,12 +127,14 @@ void zz_dissect_packet(zz_handler *zz, const struct pcap_pkthdr *packet_header,
     }
 
     /* skip unwanted access points */
+    #ifdef DEBUG
     if (!bss->is_allowed) {
         if (!is_beacon) {
             log_ts("%s @ %s - Skipping unwanted BSSID", station_str, bssid_str);
         }
         return;
     }
+    #endif
 
     /* save a beacon (just once per bss) */
     if (is_beacon) {

commit 9634cffc95f9082a246e7b29a9174a2dff72ae8b

Fix bug introduced by ccd0a66611dc59c8a92cd947eb600a08193ef9f0

src/dissector.c
@@ -127,14 +127,14 @@ void zz_dissect_packet(zz_handler *zz, const struct pcap_pkthdr *packet_header,
     }
 
     /* skip unwanted access points */
-    #ifdef DEBUG
     if (!bss->is_allowed) {
+        #ifdef DEBUG
         if (!is_beacon) {
             log_ts("%s @ %s - Skipping unwanted BSS traffic", station_str, bssid_str);
         }
+        #endif
         return;
     }
-    #endif
 
     /* save a beacon (just once per bss) */
     if (is_beacon) {