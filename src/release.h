c573a513e937879c8a1cd8832c9f8d55b8c84f47
Andrea Cardaci <cyrus.and@gmail.com>
2021-12-14 18:08:04 +0100

    Bump version

--- src/release.h
+++ src/release.h
@@ -1,7 +1,7 @@
 #ifndef ZZ_RELEASE_H
 #define ZZ_RELEASE_H
 
-#define ZZ_VERSION "0.2.1"
+#define ZZ_VERSION "0.3.0"
 #define ZZ_DESCRIPTION "Automated DeAuth attack"
 #define ZZ_AUTHOR "Andrea Cardaci <cyrus.and@gmail.com>"
 #define ZZ_YEAR "2021"
